28c1ab3800ed6c81bfe1bdf2a08d7b65ae550737
paulhung554 <150610055+paulhung554@users.noreply.github.com>
2025-09-21 14:25:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 
 # app.py
 from flask import Flask, jsonify
+from flask_cors import CORS
 import random
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/temperature', methods=['GET'])
 def get_temperature():
