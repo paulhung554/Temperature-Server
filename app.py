7a995f4f73e6d04737fffe117ede38ee7816cfe1
paulhung554 <150610055+paulhung554@users.noreply.github.com>
2025-10-05 13:34:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 
 # app.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import random
 import os
 
 app = Flask(__name__)
 CORS(app)
+lastesttempeturedata = None
 
 @app.route('/temperature', methods=['GET'])
 def get_temperature():
     # Mocked temperature data
-    temperature = round(random.uniform(20, 30), 2)
-    return jsonify({"temperature": temperature})
-
+    if lastesttempeturedata :
+        return jsonify({"temperature": lastesttempeturedata.get('temperature')})
+    return jsonify({"temperature": "Not Availablable"})
+        
+    
+@app.route('/temperature', methods=['POST'])
+def receive_sensordata():
+    # Mocked temperature data
+    global lastesttempeturedata 
+    data = request.get_json()
+    lastesttempeturedata = data
+    return 
+    
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
