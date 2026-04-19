724c03dd9e4fc8a3d863e91d32dd004b63e0f83f
paulhung554 <150610055+paulhung554@users.noreply.github.com>
2026-04-19 14:25:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ plc_data = {
 
 # Configuration
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
-ALERT_EMAIL = "paul.hung@se.com"
+ALERT_EMAIL = "paulhung554@gmail.com"
 
 # Threshold storage (in-memory for now, can be upgraded to database)
 threshold_config = {
